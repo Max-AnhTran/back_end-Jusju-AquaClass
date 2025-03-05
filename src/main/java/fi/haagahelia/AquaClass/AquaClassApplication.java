9306937e4ba90e0d7b7657be9fe4b9f25ccd3114
bhm349@myy.haaga-helia.fi <bhm349@myy.haaga-helia.fi>
2025-03-05 14:41:58 +0200

    first commit

---
+++ src/main/java/fi/haagahelia/AquaClass/AquaClassApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.AquaClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/AquaClass/AquaClassApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.AquaClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
